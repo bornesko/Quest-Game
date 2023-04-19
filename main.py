b22e66ba9c6e0f466f2695689df94a36973a61ea
SandroLesar <131313748+SandroLesar@users.noreply.github.com>
2023-04-19 17:35:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import PySimpleGUI as sg
 
-txt1 = sg.Text jfnksdfnkj
+######Slide0################################################################################################################
+font1 = ("MS Serif", 15)
+font2= ("MS Serif", 35)
+txtS01= sg.Text('Welcome to ####', font=font2)
+txtS02= sg.Text('Please enter your name!')
+inpS01= sg.Input('',size= (30,None), key = 'INP_S01')
+btnS01= sg.Button('Continue', key = 'BTN_S01')
+layout_frS0= [[txtS01], [txtS02,inpS01], [btnS01]]
+frmS0= sg.Frame('', layout_frS0,font=font1,  element_justification='c', size=(1920,1080), background_color = '#4B3619', key= 'FRMS0')
+############################################################################################################################
+layout = [[frmS0]]
 
-layout = [[]]
-
-window = sg.Window('', layout)
+window = sg.Window('Quest Game', layout, font=font1,  element_justification='c', background_color = '#4B3619').Finalize()
+window.Maximize()
 
 while True:
     event, values = window.read()
