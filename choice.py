8b4a849446b463c7e8a042722f0da3dbe898490a
bornesko <borilnestorov@gmail.com>
2023-04-30 04:16:27 +0300

    Fuck These Games

---
+++ choice.py
@@ -0,0 +1,226 @@
+import PySimpleGUI as sg
+
+img01 = sg.Image ('TestImage.png')
+font01 = ('MS Serif', 12)
+
+txt01 = sg.Text ('Choose Carefully!\n\n',font = font01, background_color = '#4B3619')
+btn01 = sg.Button ('Mushroom', size = (12,3), enable_events=True, key = 'BTN01')
+btn02 = sg.Button ('Werewolf', size = (12,3) ,enable_events=True, key = 'BTN02')
+btn03 = sg.Button ('Spider', size = (12,3) ,enable_events=True, key = 'BTN03')
+btn04 = sg.Button ('Basilisk', size = (12,3) ,enable_events=True, key = 'BTN04')
+btn05 = sg.Button ('Cerberus', size = (12,3) ,enable_events=True, key = 'BTN05')
+btn06 = sg.Button ('Fairy', size = (12,3) ,enable_events=True, key = 'BTN06')
+btn07 = sg.Button ('Fly Trap', size = (12,3) ,enable_events=True, key = 'BTN07')
+btn08 = sg.Button ('Floating Eye', size = (12,3) ,enable_events=True, key = 'BTN08')
+btn09 = sg.Button ('Goblin', size = (12,3) ,enable_events=True, key = 'BTN09')
+btn10 = sg.Button ('Ogre', size = (12,3) ,enable_events=True, key = 'BTN10')
+txt02 = sg.Text ('\n\nYou have made you choice wisely',font = font01, background_color = '#4B3619', visible = False)
+btn_exit = sg.Button ('Continue', size = (12,3), visible = False, key = 'BTN_EX')
+
+layout_fr = [[img01], [txt01], [btn01, btn02, btn03, btn04, btn05], [btn06, btn07, btn08, btn09, btn10], [txt02], [btn_exit]]
+frm01 = sg.Frame('', layout_fr, visible = True, size=(1920,1080), element_justification = 'c', background_color = '#4B3619', key = 'FRM01')
+
+layout = [[frm01]]
+
+btn01_pressed = False
+btn02_pressed = False
+btn03_pressed = False
+btn04_pressed = False
+btn05_pressed = False
+btn06_pressed = False
+btn07_pressed = False
+btn08_pressed = False
+btn09_pressed = False
+btn10_pressed = False
+btns_pressed = 0
+
+window = sg.Window('', layout, element_justification='c', background_color = '#4B3619').Finalize()
+window.Maximize()
+
+while True:
+    event, values = window.read()
+    
+    if event == 'BTN01':
+        btn01.update(button_color = 'grey', disabled = True)
+        btn01_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+            
+    elif event == 'BTN02':
+        btn02.update(button_color = 'grey', disabled = True)
+        btn02_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+ 
+    elif event == 'BTN03':
+        btn03.update(button_color = 'grey', disabled = True)
+        btn03_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+    
+    elif event == 'BTN04':
+        btn04.update(button_color = 'grey', disabled = True)
+        btn04_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+    
+    elif event == 'BTN05':
+        btn05.update(button_color = 'grey', disabled = True)
+        btn05_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+
+    elif event == 'BTN06':
+        btn06.update(button_color = 'grey', disabled = True)
+        btn06_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+      
+    elif event == 'BTN07':
+        btn07.update(button_color = 'grey', disabled = True)
+        btn07_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+   
+    elif event == 'BTN08':
+        btn08.update(button_color = 'grey', disabled = True)
+        btn08_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+  
+    elif event == 'BTN09':
+        btn09.update(button_color = 'grey', disabled = True)
+        btn09_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+        
+    elif event == 'BTN10':
+        btn10.update(button_color = 'grey', disabled = True)
+        btn10_pressed = True
+        btns_pressed +=1
+        if btns_pressed == 3:
+            txt02.update(visible = True)
+            btn01.update(disabled = True)
+            btn02.update(disabled = True)
+            btn03.update(disabled = True)
+            btn04.update(disabled = True)
+            btn05.update(disabled = True)
+            btn06.update(disabled = True)
+            btn07.update(disabled = True)
+            btn08.update(disabled = True)
+            btn09.update(disabled = True)
+            btn10.update(disabled = True)
+            btn_exit.update(visible = True)
+    
+    elif event == sg.WIN_CLOSED or event == 'BTN_EX':
+        break
+
+window.close()
---
+++ hangman.py
@@ -0,0 +1,81 @@
+import PySimpleGUI as sg
+import random
+
+# The words, which are going to be present at the hangman
+list_words = ['hello']
+
+#Choosing a random word
+word_random = random.choice(list_words)
+
+img01 = sg.Image('TestImage.png')
+
+font01 = ('MS Serif', 12)
+font02 = ('MS Serif', 15)
+
+guesses = 6
+
+txt_empty = sg.Text('',font = font01, background_color = '#4B3619')
+txt_empty2 = sg.Text('',font = font01, background_color = '#4B3619')
+
+txt01 = sg.Text('Guess the word: ', font = font02, background_color = '#4B3619')
+txt02 = sg.Text('_ '*len(word_random), font = font02, background_color = '#4B3619', key = 'WORD_1')
+txt03 = sg.Text('Guess a letter: ', font = font01, background_color = '#4B3619')
+inp01 = sg.Input('', size = (1,None), font = font01, key = 'INP_01')
+btn01 = sg.Button('Guess', size = (10,2), key = 'BTN_01')
+txt04 = sg.Text(f'\n\nYou have {guesses} left', font = font01, background_color = '#4B3619')
+
+layout_frm_1 = [[img01], [txt01, txt02], [txt_empty], [txt03, inp01], [txt_empty2], [btn01], [txt04]]
+frm01 = sg.Frame('', layout_frm_1, visible = True, size=(1920,1080), element_justification = 'c', background_color = '#4B3619', key = 'FRM_1')
+
+layout = [[frm01]]
+
+guessed_letters = []
+
+
+window = sg.Window('', layout, element_justification='c', background_color = '#4B3619').Finalize()
+window.Maximize()
+
+while True:
+    event, values = window.read()
+    
+    if event == 'BTN_01':
+        guessed_l = values['INP_01']
+        
+        # Check to see if the letter is already being used
+        if guessed_l in guessed_letters:
+            sg.popup('You have already tried this letter', text_color='black', background_color = '#cba331', font = font01)
+            continue
+        
+        guessed_letters.append(guessed_l)
+
+        # Check if the letter is in the word
+        if guessed_l in word_random:
+            letter_display = ''
+            # Update the txt02 with either an empty space or a guessed letter
+            for letter in word_random:
+                if letter in guessed_letters:
+                    letter_display += letter + ''
+                else:
+                    letter_display += '_ '
+            txt02.update(letter_display)
+            
+            # Check if there are spaces left. If not - The player wins
+            if '_ 'not in letter_display:
+                sg.popup('You win!', text_color='black', background_color = '#cba331', font = font01)
+                break
+        
+        else:
+            # Making a statement if the letter is incorrect
+            guesses -= 1
+            sg.popup('Wrong Guess!', text_color='black', background_color = '#cba331', font = font01)
+            txt04.update(f'\n\nYou have {guesses} left')
+            
+            # if there are no more guesses left, the player looses
+            if guesses == 0:
+                sg.popup(f'You lose! The word was {word_random}', text_color='black', background_color = '#cba331', font = font01)
+                break
+    
+    elif event == sg.WINDOW_CLOSED:
+        break
+
+window.close()
